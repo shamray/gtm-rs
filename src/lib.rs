51d377f9178861bccfb295f12538a0631a8687ed
Volodymyr Shamray <shamray@outlook.com>
2026-07-21 17:37:34 +0300

    Add param to query

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
-use axum::{response::IntoResponse, routing::get};
+use axum::extract::Query;
+use axum::response::{IntoResponse, Json};
+use axum::routing::get;
+
+use serde::Deserialize;
 use serde_json::json;
 
 pub fn app() -> axum::Router {
     axum::Router::new().route("/hello", get(hello_world))
 }
 
-async fn hello_world() -> impl IntoResponse {
-    axum::response::Json(json!({"response": "Hello, world"}))
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn hello_world(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    let name = params.name.as_deref().unwrap_or("world");
+    Json(json!({"response": format!("Hello, {name}")}))
 }
--- tests/test_hello.rs
+++ tests/test_hello.rs
@@ -4,7 +4,7 @@ use anyhow::Result;
 use serde_json::json;
 
 #[tokio::test]
-async fn test_hello() -> Result<()> {
+async fn test_hello_no_name() -> Result<()> {
     let addr = common::spawn_test_server().await?;
     let hc = httpc_test::new_client(&format!("http://{addr}"))?;
     let response = hc.do_get("/hello").await?;
@@ -12,6 +12,19 @@ async fn test_hello() -> Result<()> {
     let json = response.json_body()?;
 
     assert_eq!(json!({"response": "Hello, world"}), json);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_hello_with_name() -> Result<()> {
+    let addr = common::spawn_test_server().await?;
+    let hc = httpc_test::new_client(&format!("http://{addr}"))?;
+    let response = hc.do_get("/hello?name=Rusty").await?;
+
+    let json = response.json_body()?;
+
+    assert_eq!(json!({"response": "Hello, Rusty"}), json);
 
     Ok(())
 }
