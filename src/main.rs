70ff4fe8ffe6174af53658cdf77bbdcc2b0569d4
Volodymyr Shamray <shamray@outlook.com>
2026-07-21 15:15:34 +0300

    Refactor hello world

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-use std::net::SocketAddr;
-
-use axum::routing::get;
-use serde_json::{Value, json};
+use axum::{response::IntoResponse, routing::get};
+use serde_json::json;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let routes = axum::Router::new().route("/hello", get(hello_world));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    println!("Listening on {addr}");
-    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
 
-    axum::serve(listener, routes).await.unwrap();
+    axum::serve(listener, routes).await?;
+
+    Ok(())
 }
 
-async fn hello_world() -> axum::response::Json<Value> {
+async fn hello_world() -> impl IntoResponse {
     axum::response::Json(json!({"response": "Hello, world"}))
 }
