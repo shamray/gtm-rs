f36d6037f64b5b5f810589bf8594ac12c5e219e9
Volodymyr Shamray <shamray@outlook.com>
2026-07-23 18:49:13 +0300

    Fix test

--- tests/test_hello.rs
+++ tests/test_hello.rs
@@ -33,7 +33,7 @@ async fn test_hello_with_name() -> Result<()> {
 async fn test_hello2() -> Result<()> {
     let addr = common::spawn_test_server().await?;
     let hc = httpc_test::new_client(&format!("http://{addr}"))?;
-    let response = hc.do_get("/hello/Rusty").await?;
+    let response = hc.do_get("/hello2/Rusty").await?;
 
     let json = response.json_body()?;
 
