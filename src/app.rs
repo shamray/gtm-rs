6bef3fe3e3fd5d92339910b6380aee6537a25d61
Volodymyr Shamray <shamray@outlook.com>
2026-07-23 18:44:12 +0300

    Add client error type

--- src/app.rs
+++ src/app.rs
@@ -8,9 +8,10 @@ use serde_json::json;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
 use tracing::info;
+use uuid::Uuid;
 
 use crate::model::ModelController;
-use crate::web;
+use crate::{Error, web};
 
 pub fn app() -> anyhow::Result<axum::Router> {
     let mc = ModelController::new()?;
@@ -30,7 +31,28 @@ pub fn app() -> anyhow::Result<axum::Router> {
 
 async fn main_response_mapper(res: Response) -> Response {
     info!("{:?}", res);
-    res
+
+    let uuid = Uuid::new_v4();
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error" : {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+            info!("{:?}", client_error_body);
+
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    info!("server log line - {uuid} - Error - {service_error:?}");
+
+    error_response.unwrap_or(res)
 }
 
 fn routes_hello() -> axum::Router {
--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@ use tracing::error;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Clone, Debug, thiserror::Error, strum_macros::AsRefStr)]
 pub enum Error {
     #[error("login failed")]
     LoginFail,
@@ -21,6 +21,36 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         error!("{:<12} - {self:?}", "   ");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+        response.extensions_mut().insert(self);
+        response
     }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+            Self::AuthFailTokenWrongFormat | Self::AuthFailNoAuthTokenCookie => {
+                (StatusCode::FORBIDDEN, ClientError::NO_AUTH)
+            }
+            Self::TicketDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
